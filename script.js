3cfb48823546bb98ea771e448930577ea88229db
Samuel Amphoux <amphoux.s@hotmail.fr>
2023-01-24 17:29:53 +0100

    Changing to initial state

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write game code
+// Set the scores to 0
+const score1 = document.querySelector('#score--0');
+score1.textContent = 0;
+const score2 = document.querySelector('#score--1');
+score2.textContent = 0;
