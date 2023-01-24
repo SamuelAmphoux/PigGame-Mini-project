a7a75d4462f63cf65348f11105821b9ee0c05da4
Samuel Amphoux <amphoux.s@hotmail.fr>
2023-01-24 17:40:58 +0100

    Created the hidden css class

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ const score1 = document.querySelector('#score--0');
 score1.textContent = 0;
 const score2 = document.querySelector('#score--1');
 score2.textContent = 0;
+
+const dice = document.querySelector('.dice');
+dice.classList.add('hidden');
